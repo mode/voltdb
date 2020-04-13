94ed8e7fa0a23ea56b3e30b335457df2e8723fc0
Ryan Kennedy <ryan.kennedy@modeanalytics.com>
2020-04-13 09:21:46 -0700

    Replace stubbed PERCENTILE_N with naive implementation
    Replaces the previously stubbed PERCENTILE_N methods with a naive
    implementation. The naive version implements value interpolation
    following NumPy and Microsoft Excel, backed by an STL vector.
    
    This implementation works so long as there are no more than
    ~1M values in any VoltDB partition. Beyond that the implementation
    is likely to run into VoltDB errors due to the 8MB VARBINARY
    limits. A subsequent PR will introduce more efficient (but
    will likely trade off accuracy) means of implementing percentiles
    for those cases.

--- tests/frontend/org/voltdb/regressionsuites/TestPercentileSuite.java
+++ tests/frontend/org/voltdb/regressionsuites/TestPercentileSuite.java
@@ -1,97 +1,247 @@
 package org.voltdb.regressionsuites;
 
+import com.google_voltpatches.common.collect.ImmutableList;
+
+import java.lang.Byte;
 import java.lang.Exception;
+import java.lang.Short;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 import org.voltdb.BackendTarget;
+import org.voltdb.VoltTable;
 import org.voltdb.client.Client;
 import org.voltdb.client.ClientResponse;
+import org.voltdb.client.VoltBulkLoader.VoltBulkLoader;
 import org.voltdb.compiler.VoltProjectBuilder;
 
 public class TestPercentileSuite extends RegressionSuite {
-    public TestPercentileSuite(String name) {
+    private static final String UNSUPPORTED_COLUMNS_TABLE = "unsupported_column_types";
+    private static final List<String> UNSUPPORTED_COLUMN_NAMES =
+            ImmutableList.of("vb", "vc", "vb_inline", "vc_inline", "gg", "gp");
+
+    private static final List<String> SUPPORTED_COLUMNS_TABLES = ImmutableList.of(
+            "replicated_table", "partitioned_table");
+    private static final List<String> SUPPORTED_COLUMN_NAMES =
+            ImmutableList.of("bi", "ii", "si", "ti", "dd", "ff", "ts");
+
+    private static final double DEFAULT_ACCURACY_GOAL = 0.0001 / 100; // 0.0001%
+
+    private Client client;
+
+    public TestPercentileSuite(String name) throws IOException {
         super(name);
     }
 
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        client = getClient();
+    }
+
+    public void testMedianCases() throws Exception {
+        for (String tableName : SUPPORTED_COLUMNS_TABLES) {
+            // Clear the table before we get started. Just in case.
+            callProcedure("@AdHoc", "DELETE FROM " + tableName);
+
+            // Insert some test data.
+            callProcedure(tableName + ".Insert", 1, 1, 1, 1, 1, 1, 1, 1);
+            callProcedure(tableName + ".Insert", 2, 2, 2, 2, 2, 2, 2, 2);
+            callProcedure(tableName + ".Insert", 3, 3, 3, 3, 3, 3, 3, 3);
+            callProcedure(tableName + ".Insert", 4, 4, 4, 4, 4, 4, 4, 4);
+
+            VoltTable t = callProcedure("@AdHoc", "select median(ff) from " + tableName).getResults()[0];
+            assertTrue(t.advanceRow());
+            assertDoubleEquals(t, 0, 2.5, DEFAULT_ACCURACY_GOAL, "select median(ff) from " + tableName);
+
+            callProcedure(tableName + ".Insert", 5, 10, 10, 10, 10, 10, 10, 10);
+            t = callProcedure("@AdHoc", "select median(ff) from " + tableName).getResults()[0];
+            assertTrue(t.advanceRow());
+            assertDoubleEquals(t, 0, 3.0, DEFAULT_ACCURACY_GOAL, "select median(ff) from " + tableName);
+        }
+    }
+
+    public void testUnsupportedDataTypes() throws Exception {
+        for (String columnName : UNSUPPORTED_COLUMN_NAMES) {
+            for (PercentileMethod percentileMethod : PercentileMethod.values()) {
+                verifyStmtFails(client,
+                        String.format("SELECT %s(%s) FROM %s", percentileMethod, columnName, UNSUPPORTED_COLUMNS_TABLE),
+                        "incompatible data type in operation");
+            }
+        }
+    }
+
+    public void testNullResults() throws Exception {
+        // Test all permutations of table types (partitioned and replicated), all
+        // supported column types, and all percentile methods.
+        for (String tableName : SUPPORTED_COLUMNS_TABLES) {
+            // Clear the table before we get started. Just in case.
+            callProcedure("@AdHoc", "DELETE FROM " + tableName);
+
+            // Insert a row with all null values (excepting the primary key)
+            callProcedure(tableName + ".Insert", 1, null, null, null, null, null, null, null);
+
+            for (String columnName : SUPPORTED_COLUMN_NAMES) {
+                for (PercentileMethod percentileMethod : PercentileMethod.values()) {
+                    final VoltTable vt = callProcedure("@AdHoc",
+                            String.format("select %s(%s) from %s", percentileMethod.methodName(), columnName, tableName)
+                    ).getResults()[0];
+                    assertTrue(vt.advanceRow());
+
+                    // The value returned should always be null
+                    final double value = vt.getDouble(0);
+                    assertTrue(
+                            String.format("method=%s; column=%s; table=%s",
+                                    percentileMethod.methodName(), columnName, tableName),
+                            vt.wasNull());
+                }
+            }
+        }
+    }
+
+    public void testMixedNullResults() throws Exception {
+        // Test all permutations of table types (partitioned and replicated), all
+        // supported column types, and all percentile methods.
+        for (String tableName : SUPPORTED_COLUMNS_TABLES) {
+            // Clear the table before we get started. Just in case.
+            callProcedure("@AdHoc", "DELETE FROM " + tableName);
+
+            // Insert a row with all null values (excepting the primary key)
+            callProcedure(tableName + ".Insert", 1, null, null, null, null, null, null, null);
+
+            // Insert two rows with non-null values
+            callProcedure(tableName + ".Insert", 2, 0, 0, 0, 0, 0, 0, 0);
+            callProcedure(tableName + ".Insert", 3, 100, 100, 100, 100, 100, 100, 100);
+
+            for (String columnName : SUPPORTED_COLUMN_NAMES) {
+                for (PercentileMethod percentileMethod : PercentileMethod.values()) {
+                    final VoltTable vt = callProcedure("@AdHoc",
+                            String.format("select %s(%s) from %s", percentileMethod.methodName(), columnName, tableName)
+                    ).getResults()[0];
+                    assertTrue(vt.advanceRow());
+
+                    // The value returned should always match the percentile
+                    assertDoubleEquals(vt, 0, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL,
+                            String.format("method=%s; column=%s; table=%s",
+                                    percentileMethod.methodName(),
+                                    columnName,
+                                    tableName));
+                }
+            }
+        }
+    }
+
+    public void testEmptyResults() throws Exception {
+        // Test all permutations of table types (partitioned and replicated), all
+        // supported column types, and all percentile methods.
+        for (String tableName : SUPPORTED_COLUMNS_TABLES) {
+            // Clear the table before we get started. Just in case.
+            callProcedure("@AdHoc", "DELETE FROM " + tableName);
+
+            for (String columnName : SUPPORTED_COLUMN_NAMES) {
+                for (PercentileMethod percentileMethod : PercentileMethod.values()) {
+                    final VoltTable vt = callProcedure("@AdHoc",
+                            String.format("select %s(%s) from %s", percentileMethod.methodName(), columnName, tableName)
+                    ).getResults()[0];
+                    assertTrue(vt.advanceRow());
+
+                    // The value returned should always be null
+                    final double value = vt.getDouble(0);
+                    assertTrue(
+                            String.format("method=%s; column=%s; table=%s",
+                                    percentileMethod.methodName(), columnName, tableName),
+                            vt.wasNull());
+                }
+            }
+        }
+    }
+
     public void testAsTableAgg() throws Exception {
-        Client client = getClient();
+        // Test all permutations of table types (partitioned and replicated), all
+        // supported column types, and all percentile methods.
+        for (String tableName : SUPPORTED_COLUMNS_TABLES) {
+            // Clear the table before we get started. Just in case.
+            callProcedure("@AdHoc", "DELETE FROM " + tableName);
 
-        ClientResponse med_cr = client.callProcedure("@AdHoc", "SELECT MEDIAN(bi) AS int_col, MEDIAN(dd) AS double_col FROM p;");
-        assertEquals(med_cr.getStatus(), ClientResponse.SUCCESS);
-        assertTrue(med_cr.getResults()[0].advanceRow());
-        assertEquals(50.0, med_cr.getResults()[0].getDouble("int_col"));
-        assertEquals(50.0, med_cr.getResults()[0].getDouble("double_col"));
+            // Insert 101 rows with incrementing values
+            for (int i = 0; i <= 100; i++) {
+                callProcedure(tableName + ".Insert", i, i, i, i, i, i, i, i);
+            }
 
-        ClientResponse p1_cr = client.callProcedure("@AdHoc", "SELECT PERCENTILE_1(bi) AS int_col, PERCENTILE_1(dd) AS double_col FROM p;");
-        assertEquals(p1_cr.getStatus(), ClientResponse.SUCCESS);
-        assertTrue(p1_cr.getResults()[0].advanceRow());
-        assertEquals(1.0, p1_cr.getResults()[0].getDouble("int_col"));
-        assertEquals(1.0, p1_cr.getResults()[0].getDouble("double_col"));
+            for (PercentileMethod percentileMethod : PercentileMethod.values()) {
+                final String sql = String.format("select %1$s(bi), %1$s(ii), %1$s(si), %1$s(ti), %1$s(dd), %1$s(ff), %1$s(ts) from %2$s", percentileMethod.methodName(), tableName);
+                final VoltTable vt = callProcedure("@AdHoc", sql).getResults()[0];
+                assertTrue(vt.advanceRow());
 
-        ClientResponse p5_cr = client.callProcedure("@AdHoc", "SELECT PERCENTILE_5(bi) AS int_col, PERCENTILE_5(dd) AS double_col FROM p;");
-        assertEquals(p5_cr.getStatus(), ClientResponse.SUCCESS);
-        assertTrue(p5_cr.getResults()[0].advanceRow());
-        assertEquals(5.0, p5_cr.getResults()[0].getDouble("int_col"));
-        assertEquals(5.0, p5_cr.getResults()[0].getDouble("double_col"));
+                final String message = "percentile = " + percentileMethod.getPercentile() + "; table = " + tableName;
+                assertDoubleEquals(vt, 0, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL, message + "; column = bi"); // ff
+                assertDoubleEquals(vt, 1, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL, message + "; column = ii"); // ff
+                assertDoubleEquals(vt, 2, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL, message + "; column = si"); // ff
+                assertDoubleEquals(vt, 3, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL, message + "; column = ti"); // ff
+                assertDoubleEquals(vt, 4, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL, message + "; column = dd"); // ff
+                assertDoubleEquals(vt, 5, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL, message + "; column = ff"); // ff
+                assertDoubleEquals(vt, 6, percentileMethod.getPercentile(), DEFAULT_ACCURACY_GOAL, message + "; column = ts"); // ff
+            }
+        }
+    }
 
-        ClientResponse p25_cr = client.callProcedure("@AdHoc", "SELECT PERCENTILE_25(bi) AS int_col, PERCENTILE_25(dd) AS double_col FROM p;");
-        assertEquals(p25_cr.getStatus(), ClientResponse.SUCCESS);
-        assertTrue(p25_cr.getResults()[0].advanceRow());
-        assertEquals(25.0, p25_cr.getResults()[0].getDouble("int_col"));
-        assertEquals(25.0, p25_cr.getResults()[0].getDouble("double_col"));
+    public void testLargeTable() throws Exception {
+        // Test all permutations of table types (partitioned and replicated)
+        for (String tableName : SUPPORTED_COLUMNS_TABLES) {
+            // Clear the table before we get started. Just in case.
+            callProcedure("@AdHoc", "DELETE FROM " + tableName);
 
-        ClientResponse p75_cr = client.callProcedure("@AdHoc", "SELECT PERCENTILE_75(bi) AS int_col, PERCENTILE_75(dd) AS double_col FROM p;");
-        assertEquals(p75_cr.getStatus(), ClientResponse.SUCCESS);
-        assertTrue(p75_cr.getResults()[0].advanceRow());
-        assertEquals(75.0, p75_cr.getResults()[0].getDouble("int_col"));
-        assertEquals(75.0, p75_cr.getResults()[0].getDouble("double_col"));
+            VoltBulkLoader loader = client.getNewBulkLoader(tableName, 10_000, null);
 
-        ClientResponse p95_cr = client.callProcedure("@AdHoc", "SELECT PERCENTILE_95(bi) AS int_col, PERCENTILE_95(dd) AS double_col FROM p;");
-        assertEquals(p95_cr.getStatus(), ClientResponse.SUCCESS);
-        assertTrue(p95_cr.getResults()[0].advanceRow());
-        assertEquals(95.0, p95_cr.getResults()[0].getDouble("int_col"));
-        assertEquals(95.0, p95_cr.getResults()[0].getDouble("double_col"));
+            // Insert 1,000,000 rows with incrementing values
+            for (int i = 0; i <= 1_000_000; i++) {
+                loader.insertRow(i, i, i, i, i % Short.MAX_VALUE, i % Byte.MAX_VALUE, i, i, i);
+            }
 
-        ClientResponse p99_cr = client.callProcedure("@AdHoc", "SELECT PERCENTILE_99(bi) AS int_col, PERCENTILE_99(dd) AS double_col FROM p;");
-        assertEquals(p99_cr.getStatus(), ClientResponse.SUCCESS);
-        assertTrue(p99_cr.getResults()[0].advanceRow());
-        assertEquals(99.0, p99_cr.getResults()[0].getDouble("int_col"));
-        assertEquals(99.0, p99_cr.getResults()[0].getDouble("double_col"));
+            loader.drain();
+            loader.close();
+
+            // We just want to know that the query will succeed with such a large dataset
+            callProcedure("@AdHoc", "select median(bi) from " + tableName);
+        }
     }
 
     static public junit.framework.Test suite() {
         VoltServerConfig config = null;
         MultiConfigSuiteBuilder builder = new MultiConfigSuiteBuilder(TestPercentileSuite.class);
         VoltProjectBuilder project = new VoltProjectBuilder();
+        project.setRssLimit("90%");
         final String literalSchema =
-                "CREATE TABLE r ( " +
+                "CREATE TABLE replicated_table ( " +
                 "pk integer primary key not null, " +
                 "bi bigint, " +
                 "ii integer, " +
                 "si smallint, " +
                 "ti tinyint, " +
                 "dd decimal, " +
+                "ff float, " +
                 "ts timestamp " +
                 ");" +
-                "CREATE TABLE p ( " +
+                "CREATE TABLE partitioned_table ( " +
                 "pk integer primary key not null, " +
                 "bi bigint, " +
                 "ii integer, " +
                 "si smallint, " +
                 "ti tinyint, " +
                 "dd decimal, " +
+                "ff float, " +
                 "ts timestamp " +
                 "); " +
-                "partition table p on column pk;" +
-                "CREATE TABLE unsupported_column_types ( " +
+                "partition table partitioned_table on column pk;" +
+                "CREATE TABLE " + UNSUPPORTED_COLUMNS_TABLE + " ( " +
                 "vb varbinary(256), " +
                 "vc varchar(256)," +
                 "vb_inline varbinary(4), " +
                 "vc_inline varchar(4), " +
-                "ff float " +
+                "gg geography," +
+                "gp geography_point," +
                 ");";
         try {
             project.addLiteralSchema(literalSchema);
@@ -112,5 +262,44 @@ public class TestPercentileSuite extends RegressionSuite {
         builder.addServerConfig(config);
 
         return builder;
+    }
+
+    private ClientResponse callProcedure(String procedure, Object... values) throws Exception {
+        final ClientResponse response = getClient().callProcedureWithTimeout(60_000, procedure, values);
+        assertEquals(ClientResponse.SUCCESS, response.getStatus());
+        return response;
+    }
+
+    private void assertDoubleEquals(VoltTable table, int column, double expectedValue, double acceptableRange, String message) {
+        double actualValue = table.getDouble(column);
+        assertFalse(message, table.wasNull());
+
+        double percentDifference = Math.abs(actualValue - expectedValue) / expectedValue;
+        assertTrue(String.format("%s: %f is more than %f from %f", message, actualValue, acceptableRange, expectedValue),
+                acceptableRange >= percentDifference);
+    }
+
+    public static enum PercentileMethod {
+        MEDIAN(50.0),
+        PERCENTILE_1(1.0),
+        PERCENTILE_5(5.0),
+        PERCENTILE_25(25.0),
+        PERCENTILE_75(75.0),
+        PERCENTILE_95(95.0),
+        PERCENTILE_99(99.0);
+
+        private final double percentile;
+
+        private PercentileMethod(double percentile) {
+            this.percentile = percentile;
+        }
+
+        public String methodName() {
+            return name();
+        }
+
+        public double getPercentile() {
+            return percentile;
+        }
     }
 }
